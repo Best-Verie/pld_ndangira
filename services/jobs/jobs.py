7f681ba17cdc579b9c7917fba53670ff363c5878
Best-Verie <irabestverie2016@gmail.com>
2024-03-30 23:05:04 +0200

    Jobs registration Service

--- services/jobs/jobs.py
+++ services/jobs/jobs.py
@@ -7,4 +7,21 @@ def createJob():
     company = input("Company: ")
     location = input("Location: ")
     salary = input("Salary: ")
+    category = input("Category: ")
+
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT title FROM users WHERE title='{Job.title}'")
+    if cursor.fetchone() is not None:
+        print("\nTitle already taken")
+        return
+
+    cursor.close()
+
+    if connection.is_connected():
+        query = ("INSERT INTO jobs(title, description, company, location, salary, category) "
+                 "VALUES ('{job.title}', '{job.description}', '{job.company}', '{job.location}, '{job.salary}', '{job.category}')")
+
+        connection.cursor().execute(query)
+        connection.commit()
+        print("\nJob succesfully registered")
 
