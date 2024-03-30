ccc97dff8b614cb430507c6a24075b76acb8ca03
samuel.mugisha <samuel.mugisha@codeland.it>
2024-03-30 14:47:07 +0200

    add more options

--- main.py
+++ main.py
@@ -16,6 +16,38 @@ def print_menu():
     print("| 8. Exit                                                                    |")
     print("==============================================================================")
 
+def manage_account():
+    print("==============================================================================")
+    print("|                   Account Management                                      |")
+    print("==============================================================================")
+    print("| 1. Change password                                                         |")
+    print("| 2. Update profile                                                           |")
+    print("| 3. Update account status                                                           |")
+    print("| 4. Go back to main menu                                                     |")
+    print("| 5. Exit                                                                    |")
+    print("==============================================================================")
+   
+    choice = input("Choose option: ")
+    while True:
+        if choice == '1':
+            print("\nChange password still under construction!\n")
+            break
+        elif choice == '2':
+            print("\nUpdate profile still under construction!\n")
+            break
+        elif choice == '3':
+            print("\nUpdate account status still under construction!\n")
+            break
+        elif choice == '4':
+            print_menu()
+            break
+        elif choice == '5':
+            print_menu()
+            break
+        else:
+            print("\nInvalid option!\n")
+            manage_account()
+
 if __name__ == '__main__':
     choice = '7'
     while True:
@@ -26,7 +58,7 @@ if __name__ == '__main__':
         elif choice == '3':
             print("\nBye!\n")
         elif choice == '4':
-            print("\nBye!\n")
+            manage_account()
         elif choice == '5':
             print("\nBye!\n")
         elif choice == '6':
