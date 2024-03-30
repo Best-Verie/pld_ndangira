da5e04e71cfa20f7bec678d8b74614532f284246
Best-Verie <irabestverie2016@gmail.com>
2024-03-30 22:59:42 +0200

    Jobs Service

--- services/jobs/jobs.py
+++ services/jobs/jobs.py
@@ -1,1 +1,10 @@
+from models.job import Job
+from db.connection import connection
+
 def createJob():
+    title = input("Title: ")
+    description = input("Description: ")
+    company = input("Company: ")
+    location = input("Location: ")
+    salary = input("Salary: ")
+
